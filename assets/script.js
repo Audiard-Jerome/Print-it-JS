2a1dd50647ebc5e49d362785381e36e5029b232a
Jerome Audiard <jerome.audiard@gmail.com>
2024-02-09 10:28:13 +0100

    add bullets points

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,8 @@ const slides = [
 	}
 ]
 
+
+
 // query selector sur la fleche gauche
 let arrow_left = document.querySelector("#banner .arrow_left")
 arrow_left.addEventListener("click", () => {
@@ -28,3 +30,24 @@ let arrow_right = document.querySelector("#banner .arrow_right")
 arrow_right.addEventListener("click", () => {
 	console.log("Vous avez cliqué sur la fleche droite")
 	});
+
+//ajout des bullets points
+
+//init diapo en cours
+let diapoEnCours = 0
+
+//recupération zone des bullets points
+let dots = document.querySelector("#banner .dots")
+//ecriture de x dots en fonction de la longueur de la liste 
+	for (let i = 0; i < slides.length; i++) {
+	// crée une div avec la classe dot.
+		let dot = document.createElement(`div`)
+		dot.classList.add(`dot`)
+		//test pour savoir si c'est la diapo en cours
+		if (diapoEnCours === i) {
+			//si vrai on ajouter la class dot_selected
+			dot.classList.add("dot_selected")
+			} 
+		//affiche les dots au bon endroit.
+		dots.appendChild(dot)
+		}
