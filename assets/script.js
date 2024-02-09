a3afad7f20bb9e2c0d5b4bc2e1f4fde420f752ac
Jerome Audiard <jerome.audiard@gmail.com>
2024-02-09 10:25:02 +0100

    Add Event Listeners on arrow

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,15 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// query selector sur la fleche gauche
+let arrow_left = document.querySelector("#banner .arrow_left")
+arrow_left.addEventListener("click", () => {
+	console.log("Vous avez cliqué sur la fleche gauche")
+	});
+
+// query selector sur la fleche gauche
+let arrow_right = document.querySelector("#banner .arrow_right")
+arrow_right.addEventListener("click", () => {
+	console.log("Vous avez cliqué sur la fleche droite")
+	});
